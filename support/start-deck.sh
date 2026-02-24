80d55d09b56843784bfdc81df69ed7bd9b03ab76
corycaean <corycaean@users.noreply.github.com>
2026-02-24 10:19:33 -0500

    edited startup script

--- support/start-deck.sh
+++ support/start-deck.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# enable compose key
+export XKB_DEFAULT_OPTIONS="lv3:ralt_switch,compose:rctrl"
+
 # The '--' is the critical part here
 cage foot -- sh -c "cd /path/to/journal && ./run.sh; exec bash" &
 
